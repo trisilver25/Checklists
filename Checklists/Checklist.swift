554198e9a622db5cd527aebbd36b835b06f6f5e3
Tristin Smith <tsmithassoc@gmail.com>
2024-01-31 21:45:40 -0500

    Updates to CheckList: - Added 2 new scenes - 1 scene is viewing each checklist - The other scene is for adding/editing checklists shown - Added multiple segues. - Now when you select a checklsit it can have multiple tasks within it.

---
+++ Checklists/Checklist.swift
@@ -0,0 +1,12 @@
+//
+//  Checklist.swift
+//  Checklists
+//
+//  Created by Tristin Smith on 1/31/24.
+//
+
+import UIKit
+
+class Checklist: NSObject {
+
+}
---
+++ Checklists/ListDetailViewController.swift
@@ -0,0 +1,8 @@
+//
+//  ListDetailViewController.swift
+//  Checklists
+//
+//  Created by Tristin Smith on 1/31/24.
+//
+
+import Foundation
